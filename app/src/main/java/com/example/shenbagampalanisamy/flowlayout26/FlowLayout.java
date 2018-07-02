40d10c1d8525c7f6a1ab6502afec1c44e43982cb
shenbagam.palanisamy <shenbagam2224@gmail.com>
2018-07-02 06:40:54 -0700

    right gravity

--- app/src/main/java/com/example/shenbagampalanisamy/flowlayout26/FlowLayout.java
+++ app/src/main/java/com/example/shenbagampalanisamy/flowlayout26/FlowLayout.java
@@ -1,8 +1,11 @@
 package com.example.shenbagampalanisamy.flowlayout26;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import java.util.ArrayList;
@@ -20,6 +23,10 @@ public class FlowLayout extends ViewGroup {
     int mVerticalSpacing;
     private int DEFAULT_HORIZONTAL_SPACING = 15;
     private int DEFAULT_VERTICAL_SPACING = 15;
+    private int mGravity = (isIcs() ? Gravity.START : Gravity.LEFT) | Gravity.TOP;
+    private int layoutDirection = FlowLayout.LAYOUT_DIRECTION_LTR;
+    final int absoluteGravity = Gravity.getAbsoluteGravity(mGravity, layoutDirection);
+    final int verticalGravity = mGravity & Gravity.VERTICAL_GRAVITY_MASK;
 
 
     public FlowLayout(Context context) {
@@ -48,11 +55,17 @@ public class FlowLayout extends ViewGroup {
 
             this.setOrientation(a.getInteger(R.styleable.FlowLayout_android_orientation, FlowLayout.HORIZONTAL));
 
+
+            int index = a.getInt(R.styleable.FlowLayout_android_gravity, -1);
+            if(index > 0) {
+                setGravity(index);
+            }
+            
+
         } finally {
             a.recycle();
         }
     }
-
     public void setOrientation(int orientation) {
         if (orientation == FlowLayout.VERTICAL) {
             this.orientation = orientation;
@@ -66,12 +79,13 @@ public class FlowLayout extends ViewGroup {
     }
 
 
+
+
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int val = this.getOrientation();
         int val_h = FlowLayout.HORIZONTAL;
-        final int sizeHeight = MeasureSpec.getSize(heightMeasureSpec) - this.getPaddingTop() - this.getPaddingBottom();
-        Log.i("size", String.valueOf(sizeHeight));
         //For Horizontal
         int childLeft = getPaddingLeft();
 
@@ -99,24 +113,21 @@ public class FlowLayout extends ViewGroup {
                         getChildMeasureSpec(heightMeasureSpec, 0, child.getLayoutParams().height));
                 int childWidth = child.getMeasuredWidth();
                 int childHeight = child.getMeasuredHeight();
-                Log.i("chileLeft", String.valueOf(childWidth));
                 lineHeight = Math.max(childHeight, lineHeight);
-                if (childWidth + childLeft + getPaddingRight() > myWidth) {
+               if (childWidth + childLeft + getPaddingRight() > myWidth) {
                     // wrap this line
                     childLeft = getPaddingLeft();
                     childTop += mVerticalSpacing + lineHeight;
                     lineHeight = childHeight;
                 }
-                childLeft += childWidth + mHorizontalSpacing;
+               childLeft += childWidth + mHorizontalSpacing;
             }
             wantedHeight += childTop + lineHeight + getPaddingBottom();
             setMeasuredDimension(myWidth, resolveSize(wantedHeight, heightMeasureSpec));
             int a=resolveSize(wantedHeight, heightMeasureSpec);
-            System.out.println(a);
-            //Log.i("hi", String.valueOf(a));
         } else {
 
-            for (int i = 0; i < getChildCount(); i++) {
+            for (int i = 0; i<getChildCount() ; i++) {
                 final View child = getChildAt(i);
                 if (child.getVisibility() == View.GONE) {
                     continue;
@@ -143,59 +154,159 @@ public class FlowLayout extends ViewGroup {
     }
 
         @Override
-        protected void onLayout ( boolean changed, int left, int top, int right, int bottom){
+        protected void onLayout ( boolean changed, int left, int top, int right, int bottom) {
             int childLeft = getPaddingLeft();
             int childTop = getPaddingTop();
+            int childRight = getPaddingRight();
             int lineHeight = 0;
             int myWidth = right - left;
-            int myHeight=bottom-top;
-            int lineWidth=0;
+            int myHeight = bottom - top;
+            int lineWidth = 0;
 
             int orient = this.getOrientation();
             int hori = FlowLayout.HORIZONTAL;
 
-            if (orient == hori) {
-                for (int i = 0; i < getChildCount(); i++) {
-                    child = getChildAt(i);
-                    if (child.getVisibility() == View.GONE) {
-                        continue;
+            if (orient == hori)
+                if (Gravity.LEFT == mGravity) {
+
+                    for (int i = 0; i < getChildCount(); i++) {
+                        child = getChildAt(i);
+                        if (child.getVisibility() == View.GONE) {
+                            continue;
+                        }
+                        int childWidth = child.getMeasuredWidth();
+                        int childHeight = child.getMeasuredHeight();
+                        lineHeight = Math.max(childHeight, lineHeight);
+                        if (childWidth + childLeft + getPaddingRight() > myWidth) {
+                            childLeft = getPaddingLeft();
+                            childTop += mVerticalSpacing + lineHeight;
+                            lineHeight = childHeight;
+                        }
+                        child.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+                        childLeft += childWidth + mHorizontalSpacing;
                     }
-                    int childWidth = child.getMeasuredWidth();
-                    int childHeight = child.getMeasuredHeight();
-                    lineHeight = Math.max(childHeight, lineHeight);
-                    if (childWidth + childLeft + getPaddingRight() > myWidth) {
+
+                    }
+                 else if (Gravity.RIGHT == mGravity) {
+
+                    View[] childList = new View[getChildCount()];
+                    int idx = 0;
+                    for (int i = getChildCount()-1; i >= 0; i--) {
+                        childList[idx] = getChildAt(i);
+                        idx++;
+                    }
+
+                    /*for (int i = 0; i < getChildCount(); i++) {
+                        child = getChildAt(i);
+                        if (child.getVisibility() == View.GONE) {
+                            continue;
+                        }
+                        int childWidth = child.getMeasuredWidth();
+                        int childHeight = child.getMeasuredHeight();
+                        lineHeight = Math.max(childHeight, lineHeight);
+                  *//*  if (childWidth + childLeft + getPaddingRight() > myWidth) {
                         childLeft = getPaddingLeft();
                         childTop += mVerticalSpacing + lineHeight;
                         lineHeight = childHeight;
-                    }
-                    child.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
-                    childLeft += childWidth + mHorizontalSpacing;
-                }
-            } else {
-                for (int i = 0; i < getChildCount(); i++) {
-                    child = getChildAt(i);
-                    if (child.getVisibility() == View.GONE) {
-                        continue;
-                    }
-                    int childWidth = child.getMeasuredWidth();
-                    int childHeight = child.getMeasuredHeight();
+                    }*//*
+                        int left1 = myWidth - childWidth;
+                        int right1 = myWidth;
+                        child.layout(left1, childTop, right1, childHeight);
+                        myWidth = left1 - mHorizontalSpacing;
+                        //right1=(right1-childWidth)-mHorizontalSpacing;
 
-                    lineWidth = Math.max(childWidth, lineWidth);
-                    if (childHeight + childTop + getPaddingBottom() > myHeight) {
-                        childTop=getPaddingTop();
-                        childLeft+= mHorizontalSpacing+lineWidth;
-                        lineWidth=childWidth;
-                    }
+                    }*/
 
-                    child.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
-                    childTop += childHeight + mVerticalSpacing;
+
+
+
+
+
+                    for (int i = 0; i < childList.length; i++) {
+                        child = childList[i];
+                        if (child.getVisibility() == View.GONE) {
+                            continue;
+                        }
+                        int childWidth = child.getMeasuredWidth();
+                        int childHeight = child.getMeasuredHeight();
+                        lineHeight = Math.max(childHeight, lineHeight);
+                        int left1 = myWidth - childWidth;
+                        int right1 = myWidth;
+                        child.layout(left1, childTop, right1, childHeight);
+                        myWidth = left1 - mHorizontalSpacing;
+                    }
                 }
+                else if(Gravity.CENTER==mGravity) {
+                    for (int i = 0; i < getChildCount(); i++) {
+                        child = getChildAt(i);
+                        if (child.getVisibility() == View.GONE) {
+                            continue;
+                        }
+                        int childWidth = child.getMeasuredWidth();
+                        int childHeight = child.getMeasuredHeight();
+                        lineHeight = Math.max(childHeight, lineHeight);
+                  /*  if (childWidth + childLeft + getPaddingRight() > myWidth) {
+                        childLeft = getPaddingLeft();
+                        childTop += mVerticalSpacing + lineHeight;
+                        lineHeight = childHeight;
+                    }*/
+                        int left1 = myWidth - childWidth;
+                        int right1 = myWidth;
+                        child.layout(left1, myHeight / 2, right1, childHeight+childHeight);
+                        myWidth = left1 - mHorizontalSpacing;
+                        //right1=(right1-childWidth)-mHorizontalSpacing;
+
+                    }
+                }
+
+
+            else {
+                    for (int i = 0; i < getChildCount(); i++) {
+                        child = getChildAt(i);
+                        if (child.getVisibility() == View.GONE) {
+                            continue;
+                        }
+                        int childWidth = child.getMeasuredWidth();
+                        int childHeight = child.getMeasuredHeight();
+
+                        lineWidth = Math.max(childWidth, lineWidth);
+                        if (childHeight + childTop + getPaddingBottom() > myHeight) {
+                            childTop = getPaddingTop();
+                            childLeft += mHorizontalSpacing + lineWidth;
+                            lineWidth = childWidth;
+                        }
+
+                        child.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+                        childTop += childHeight + mVerticalSpacing;
+                    }
+                }
+
             }
 
-        }
-
-
-
+    private static boolean isIcs() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
     }
+
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void setGravity(int gravity) {
+        if(mGravity != gravity) {
+            /*if((gravity & Gravity.RELATIVE_HORIZONTAL_GRAVITY_MASK) == 0) {
+                gravity |= isIcs() ? Gravity.START : Gravity.LEFT;
+            }
+
+            if((gravity & Gravity.VERTICAL_GRAVITY_MASK) == 0) {
+                gravity |= Gravity.TOP;
+            }*/
+
+            mGravity = gravity;
+            requestLayout();
+        }
+    }
+
+    public int getGravity() {
+        return mGravity;
+    }
+}
 
 
